66b3efb754a17ae9bc437b3a2dc1568c635c566d
Jonathan Good <github@bungogood.com>
2023-10-08 16:59:09 +0100

    Equities

--- src/bin/unique.rs
+++ src/bin/unique.rs
@@ -1,13 +1,84 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 use bkgm::{
     dice::{ALL_21, ALL_SINGLES},
+    GameState::{GameOver, Ongoing},
     Position,
 };
+use clap::Parser;
 
 /// Benchmark and test position / move generation
 
-fn unqiue() -> HashSet<Position> {
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Number of games to generate
+    #[arg(short = 'd', long = "depth")]
+    depth: usize,
+
+    /// Position
+    #[arg(short = 'p', long = "position", default_value = "4HPwATDgc/ABMA")]
+    position: String,
+
+    /// Verbose
+    #[arg(short = 'v', long = "verbose", default_value = "false")]
+    verbose: bool,
+}
+
+struct Roll {
+    children: Vec<Position>,
+    prior: f32,
+}
+
+fn equity_update(equities: &HashMap<Position, f32>) -> HashMap<Position, f32> {
+    let mut updated = equities.clone();
+
+    let mut delta = 0.0;
+    for position in equities.keys() {
+        let equity = match position.game_state() {
+            Ongoing => {
+                let mut total = 0.0;
+                for (die, n) in ALL_21 {
+                    let children = position.all_positions_after_moving(&die);
+                    total += children
+                        .iter()
+                        .map(|pos| equities[pos])
+                        .min_by(|a, b| a.partial_cmp(b).unwrap())
+                        .unwrap()
+                        * n;
+                }
+                total / 36.0
+            }
+            GameOver(result) => result.value(),
+        };
+        updated.insert(position.clone(), equity);
+        delta += (equity - equities[position]).abs();
+    }
+    println!("Delta: {}", delta);
+    updated
+}
+
+fn equity(positions: &HashSet<Position>) -> HashMap<Position, f32> {
+    let mut equities = HashMap::new();
+    for &position in positions {
+        let equity = match position.game_state() {
+            Ongoing => 0.0,
+            GameOver(result) => result.value(),
+        };
+        equities.insert(position, equity);
+    }
+
+    println!("Positions: {}", equities.len());
+    for _ in 0..200 {
+        equities = equity_update(&equities);
+    }
+    println!("Equity: {}", equities.len());
+
+    equities
+}
+
+fn unqiue(args: &Args) -> HashSet<Position> {
+    // let position = Position::from_id(&args.position).expect("Invalid position");
     let position = Position::hypergammon();
     let mut found = HashSet::new();
     let mut new_positons = vec![];
@@ -60,8 +131,10 @@ fn unqiue() -> HashSet<Position> {
 }
 
 fn main() {
+    let args = Args::parse();
     let start = std::time::Instant::now();
-    let unique = unqiue();
+    let unique = unqiue(&args);
+    equity(&unique);
     let total = unique.len();
     let dur = start.elapsed();
     let speed = total as f64 / dur.as_secs_f64();
